6ebefe00ae36b88f6b417a7f52923cee14a3f546
Marcin Kuniec <marcinkuniec@o2.pl>
2022-10-06 23:24:44 +0200

    homepage

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+
+
+
 console.log('siema');
 const name = 'Marcin' ;
 const age = 31;
@@ -9,10 +12,29 @@ i mam ${age} lat`)
 
 
 
-
-
 const about = document.querySelector('.paragraph__JS');
 
 about.innerHTML = 'pozdrówki';
 
+switch (age) {
+    case 20:
+        console.log( 'masz 20 lat')
+}
 
+
+function calculate(x) {
+    x = x + 3;
+    console.log(`tradycyjnie ${x}`);
+    return x*7;
+
+}
+
+
+console.log(calculate(25));
+
+
+
+
+const calculateFat = x => (x+3)*7;
+
+console.log(calculateFat(8));
